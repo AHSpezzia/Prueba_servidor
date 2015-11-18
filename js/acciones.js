b2ea304b8cda345e9b06712b45928a4a1f09568c
AHSpezzia <damcb164e20@gmail.com>
2015-11-18 11:56:09 -0600

    2da
    Conexion al servidor

--- js/acciones.js
+++ js/acciones.js
@@ -1,7 +1,20 @@
 // JavaScript Document
+function consult(){	
+datos="";
+	$.ayas({
+		type:"POST",
+		url:"192.168.1.30/procesar.php",
+		data: datos
+	}).done(function(mensaje){
+		alert (mensaje);
+	});
+}
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
-	
+	$("#btnconsulta").on("tap",function(){
+		alert("Preguntando...");
+		consulta();
+	});
 }); 
 });
 
